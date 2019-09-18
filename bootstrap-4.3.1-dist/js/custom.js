0a6aaec39d5aec6aa0f533cdf83a5cc4bbcb7c3d
RtjShreyD <shreyanshece1041@gmail.com>
2019-09-18 17:24:20 +0530

    POST now working with ajax also

--- bootstrap-4.3.1-dist/js/custom.js
+++ bootstrap-4.3.1-dist/js/custom.js
@@ -1,32 +1,44 @@
+var datas = "";
 
 $('#get').click(function() { //get here is the id of the button targetted
-
     console.log('working');
-
-    $.ajax({
- 
+    $.ajax({ 
         // The URL for the request
-        url: "http://localhost:8000/languages/",
-     
-            
+        url: "http://localhost:8000/languages/",      
         // Whether this is a POST or GET request
         type: "GET",
-     
         // The type of data we expect back
         dataType : "json",
     })
       // Code to run if the request succeeds (is done);
       // The response is passed to the function
-      .done(function( data ) {
-        $('h1').text( data[1].name );
+      .done(function( datas ) {
+        $('h1').text( datas[1].name );
         //  $( "<h1>" ).text( json.title ).appendTo( "body" );
         //  $( "<div class=\"content\">").html( json.html ).appendTo( "body" );
-        console.log(json);
+        console.log(datas);
       });
-
 });
 
 $("#post").click(function(){
+  $.ajax({
+    type:"POST",
+    url:"http://127.0.0.1:8000/programmers/",
+    //dataType: "json",
+    //contentType: "application/json; charset=utf-8",
+    data: {
+        "name":"Baba",
+        "languages":"http://127.0.0.1:8000/languages/3/"
+     },
+     //processData: true, 
+     success: function(response) {          
+        console.log(response);
+      }  
+  })
+});
+
+
+/*$("#post").click(function(){
   $.post("http://127.0.0.1:8000/programmers/",
   {
     name: "Donald Duck",
@@ -36,57 +48,4 @@ $("#post").click(function(){
     alert("Data: " + data + "\nStatus: " + status);
   });
 }); 
-
-
-/*$('#post').click(function() {
-
-  // $.ajax( {
-
-  //   url: "http://127.0.0.1:8000/programmers/",
- 
-  //  // The data to send (will be converted to a query string)
-  //  data: {
-  //   name: "Kantabai",
-  //   "languages[]": ["http://127.0.0.1:8000/languages/5/"]
-  // },
- 
-  //   // Whether this is a POST or GET request
-  //   type: "POST",
- 
-  //   // The type of data we expect back
-  //   dataType : "json",
-
-  //   contentType: "application/json"
-
-  // } )
-  // .done(function(response) {
-  //   console.log(response);
-  // })
-
-  $.post( "http://127.0.0.1:8000/programmers/", { 'name': "Messi", 'languages[]': [ "http://127.0.0.1:8000/languages/5/" ] } );
-
-  // fetch("http://127.0.0.1:8000/programmers/", {
-
-  //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-  //   // mode: 'cors', // no-cors, *cors, same-origin
-  //   // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-  //   // credentials: 'same-origin', // include, *same-origin, omit
-  //   headers: {
-  //       'Content-Type': 'application/json',
-  //       // 'Content-Type': 'application/x-www-form-urlencoded',
-  //   },
-  //   // redirect: 'follow', // manual, *follow, error
-  //   // referrer: 'no-referrer', // no-referrer, *client
-  //   body: JSON.stringify({
-  //       "name": "Ritiiii",
-  //       "languages": ["http://127.0.0.1:8000/languages/5/"]
-  //     }), 
-
-  // })
-  // .then(function(data) {
-
-  //   console.log(data);
-  // })
-
-})
- */
+*/
